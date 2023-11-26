96d33d02c7427240e15154bbd0a798d3e6b65c32
kendihasko <khasko16@epoka.edu.al>
2023-11-26 12:26:52 -0800

    changes to base.py code

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 """
 Contains a Base class to manage
 the id attribute of all classes that extend
 from Base.
 """
 
+# Importing libraries
 from os import path
 import json
 
@@ -12,11 +14,14 @@ import json
 class Base:
     __nb_objects = 0
 
-    def __init__(self, id=None):
-        if id is not None:
-            # If id is provided, assign it to the public instance attribute id
-            self.id = id
-        else:
-            # If id is not provided, increment __nb_objects and assign the new value to id
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+# If id is provided, assign it to the public instance attribute id
+     def __init__(self, id=None):
+
+         if id is not None:
+         self.id = id
+
+      # If id  not provided, increment and assign the new value to id 
+         else:
+             Base.__nb_objects += 1
+             self.id = Base.__nb_objects
+
