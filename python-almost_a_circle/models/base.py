0fa313f7220d3e86945c957c080003844b7f804d
kendihasko <khasko16@epoka.edu.al>
2023-11-26 12:33:34 -0800

    fix in base.py code

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-Contains a Base class to manage
-the id attribute of all classes that extend
+Base class manages
+id attribute of all classes that extend
 from Base.
 """
 
@@ -11,17 +11,18 @@ from os import path
 import json
 
 # Private class attribute
+
+
 class Base:
     __nb_objects = 0
 
-# If id is provided, assign it to the public instance attribute id
-     def __init__(self, id=None):
+# If id  provided, assign to public instance attribute id
+    def __init__(self, id=None):
 
-         if id is not None:
-         self.id = id
+        if id is not None:
+            self.id = id
 
-      # If id  not provided, increment and assign the new value to id 
-         else:
-             Base.__nb_objects += 1
-             self.id = Base.__nb_objects
-
+         # If id  not provided, increment and assign the new value to id
+            else:
+                Base.__nb_objects += 1
+                self.id = Base.__nb_objects
