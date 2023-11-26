4cf3f064ffbf0bd173f503b3f8a5af7faf36da17
kendihasko <khasko16@epoka.edu.al>
2023-11-26 12:52:58 -0800

    base.py code fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,28 +1,47 @@
-
 #!/usr/bin/python3
 
 # Importing libraries
 from os import path
 import json
 
-"""
-Base class manages
-id attribute of all classes that extend
-from Base.
-"""
-
-# Private class attribute
-
 class Base:
+    """Base class for managing id attribute.
+
+    Attributes:
+        __nb_objects (int): A private class attribute to keep track of the number of instances.
+
+    Args:
+        id (int, optional): The id to assign to the instance. If None, a new id is generated.
+
+    Attributes:
+        id (int): The public instance attribute representing the id.
+
+    Methods:
+        __init__: Initializes a new instance of the Base class.
+
+    Example:
+        Create an instance with a specific id:
+        >>> b = Base(id=5)
+        >>> print(b.id)
+        5
+
+        Create an instance with a generated id:
+        >>> b = Base()
+        >>> print(b.id)
+        1
+    """
+
     __nb_objects = 0
 
-# If id  provided, assign to public instance attribute id
     def __init__(self, id=None):
+        """Initializes a new instance of the Base class.
 
+        Args:
+            id (int, optional): The id to assign to the instance. If None, a new id is generated.
+        """
         if id is not None:
             self.id = id
-
-        # If id  not provided, increment and assign the new value to id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
