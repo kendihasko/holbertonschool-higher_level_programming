b8e6ea24f5d355e526fca914284d07891b3da2af
kendihasko <khasko16@epoka.edu.al>
2023-11-27 08:49:11 -0800

    just to watch my code in git

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,4 +108,10 @@ class Rectangle(Base):
         '''
         for _ in range(self.__height):
             print('#' * self.__width)
+    
+    def __str__(self):
+        '''
+        Returns a string representation of the Rectangle instance.
+        '''
+        return f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}'
 
