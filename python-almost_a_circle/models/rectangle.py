a9a1301714037d5cb4fca9215cd46bb5068f7279
kendihasko <khasko16@epoka.edu.al>
2023-11-27 09:39:24 -0800

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -121,3 +121,21 @@ class Rectangle(Base):
                 f'[Rectangle] ({self.id}) '
                 f'{self.__x}/{self.__y} - {self.__width}/{self.__height}'
         )
+    def update(self, *args):
+        '''
+        Assigns an argument to each attribute.
+
+        Args:
+            *args: Arguments to be assigned in order (id, width, height, x, y).
+        '''
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
