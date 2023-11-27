18e17c46e278f862859762ebed7d401cae8e9736
kendihasko <48715368+kendihasko@users.noreply.github.com>
2023-11-27 12:25:35 +0100

    adding more documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,9 +13,9 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        ...
-        """
+      '''
+      ...
+      '''
 
         if id is not None:
             # If id  provided, assign to public instance attribute id
