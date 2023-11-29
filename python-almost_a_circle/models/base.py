2a5b70f1cf3863574ef68141ef6d09b836eb2cb8
kendihasko <48715368+kendihasko@users.noreply.github.com>
2023-11-29 23:29:50 +0100

    fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,9 +40,7 @@ class Base:
     with open(filename, mode='w', encoding='utf-8') as f:
         if list_objs is None:
             return f.write(cls.to_json_string([]))
-            '''...'''
 
         json_attrs = [obj.to_dictionary() for obj in list_objs]
 
         return f.write(cls.to_json_string(json_attrs))
-        '''...'''
