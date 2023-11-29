a844adaceb2075723cd20be82505e94f4bdd676c
kendihasko <48715368+kendihasko@users.noreply.github.com>
2023-11-29 23:36:54 +0100

    Adding save_to_file method in Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -58,8 +58,11 @@ class Base:
 
         with open(filename, mode='w', encoding='utf-8') as f:
             if list_objs is None:
-                return f.write(cls.to_json_string([]))
+                return f.write(cls.to_json_string(None))
 
-            json_attrs = [obj.to_dictionary() for obj in list_objs]
+            json_attrs = []
+
+            for elem in list_objs:
+                json_attrs.append(elem.to_dictionary())
 
             return f.write(cls.to_json_string(json_attrs))
