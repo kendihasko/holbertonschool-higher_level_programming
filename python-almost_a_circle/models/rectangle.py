acd5725ba285404be8d4b8bce3e365b4673803fb
kendihasko <48715368+kendihasko@users.noreply.github.com>
2023-11-30 02:08:18 +0100

    Adding the public method def to_dictionary(self) to Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ from base import Base
 
 class Rectangle(Base):
     '''
-    Rectangle class inherits from Base.
+Rectangle class inherits from Base.
     '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
